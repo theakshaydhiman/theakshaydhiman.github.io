e908031bb8534f94bb6cde2a67b076bc51352455
Akshay Dhiman <34573958+theakshaydhiman@users.noreply.github.com>
2018-04-12 14:06:19 +0530

    0.1.2

--- lite-modal/js/lite-modal.js
+++ lite-modal/js/lite-modal.js
@@ -1,60 +1,65 @@
 /*
-*   Lite Modal v0.1.1
+*   Lite Modal v0.1.2
 *   https://github.com/theakshaydhiman/lite-modal
 *   (c) 2018 Akshay Dhiman. MIT License.
 */
 
-var lModalOverlay = document.querySelector('.lmodal-overlay');
-var lModalTrigger = document.querySelector('.lmodal-trigger');
-var lModalContent = document.querySelector('.lmodal-content');
-var lModalClose = document.getElementsByClassName('lmodal-close');
-var b = document.getElementById('body');
+(function() {
 
-// Show modal
-function showModal() {
-    b.classList.add("lModal-opened");
-    setTimeout( function() {
-        fadeIn(lModalOverlay);
-        fadeIn(lModalContent);
-    }, 100);
-}
-lModalTrigger.addEventListener('click', showModal);
+    var lModalOverlay = document.querySelector('.lmodal-overlay'),
+        lModalTrigger = document.querySelector('.lmodal-trigger'),
+        lModalContent = document.querySelector('.lmodal-content'),
+        lModalClose = document.getElementsByClassName('lmodal-close'),
+        b = document.getElementById('body'),
+        i = 0;
 
-// Close modal
-function closeModal() {
-    fadeOut(lModalOverlay);
-    fadeOut(lModalContent);
-    b.classList.remove("lModal-opened");
-}
-lModalOverlay.addEventListener('click', closeModal);
-for (var i = 0; i < lModalClose.length; i++) {
-    lModalClose[i].addEventListener('click', closeModal, false);
-}
+    // Show modal
+    function showModal() {
+        b.classList.add("lModal-opened");
+        setTimeout( function() {
+            fadeIn(lModalOverlay);
+            fadeIn(lModalContent);
+        }, 100);
+    }
+    lModalTrigger.addEventListener('click', showModal);
 
-// Fade out
-function fadeOut(el){
-    el.style.opacity = 1;
+    // Close modal
+    function closeModal() {
+        fadeOut(lModalOverlay);
+        fadeOut(lModalContent);
+        b.classList.remove("lModal-opened");
+    }
+    lModalOverlay.addEventListener('click', closeModal);
+    for (i = 0; i < lModalClose.length; i++) {
+        lModalClose[i].addEventListener('click', closeModal, false);
+    }
 
-    (function fade() {
-        if ((el.style.opacity -= 0.2) < 0) {
-            el.style.display = "none";
-        } else {
-            requestAnimationFrame(fade);
-        }
-    })();
-}
+    // Fade out
+    function fadeOut(el){
+        el.style.opacity = 1;
 
-// Fade in
-function fadeIn(el, display){
-    el.style.opacity = 0;
-    el.style.display = display || "block";
+        (function fade() {
+            if ((el.style.opacity -= 0.2) < 0) {
+                el.style.display = "none";
+            } else {
+                requestAnimationFrame(fade);
+            }
+        })();
+    }
 
-    (function fade() {
-        var val = parseFloat(el.style.opacity);
-        val += 0.2;
-        if ( val == null || val <= 1 ) {
-            el.style.opacity = val;
-            requestAnimationFrame(fade);
-        }
-    })();
-}
+    // Fade in
+    function fadeIn(el, display){
+        el.style.opacity = 0;
+        el.style.display = display || "block";
+
+        (function fade() {
+            var val = parseFloat(el.style.opacity);
+            val += 0.2;
+            if ( val == null || val <= 1 ) {
+                el.style.opacity = val;
+                requestAnimationFrame(fade);
+            }
+        })();
+    }
+
+})();
