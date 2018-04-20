52d69deb9b7587cc768ee14fc66a3d85e7fe669b
Akshay Dhiman <34573958+theakshaydhiman@users.noreply.github.com>
2018-04-20 21:31:37 +0530

    0.2.0

--- lite-modal/js/lite-modal.js
+++ lite-modal/js/lite-modal.js
@@ -1,5 +1,5 @@
 /*
-*   Lite Modal v0.1.2
+*   Lite Modal v0.2.0
 *   https://github.com/theakshaydhiman/lite-modal
 *   (c) 2018 Akshay Dhiman. MIT License.
 */
@@ -16,8 +16,9 @@
     // Show modal
     function showModal() {
         b.classList.add("lModal-opened");
+        b.style.filter = "blur(3px)";
         setTimeout( function() {
-            fadeIn(lModalOverlay);
+            fadeInOverlay(lModalOverlay);
             fadeIn(lModalContent);
         }, 100);
     }
@@ -25,9 +26,10 @@
 
     // Close modal
     function closeModal() {
-        fadeOut(lModalOverlay);
+        fadeOutOverlay(lModalOverlay);
         fadeOut(lModalContent);
         b.classList.remove("lModal-opened");
+        b.style.filter = "blur(0px)";
     }
     lModalOverlay.addEventListener('click', closeModal);
     for (i = 0; i < lModalClose.length; i++) {
@@ -36,6 +38,21 @@
 
     // Fade out
     function fadeOut(el){
+        el.style.opacity = 1;
+        var scale = 1;
+
+        (function fade() {
+            scale -= 0.01;
+            if ((el.style.opacity -= 0.2) < 0) {
+                el.style.display = "none";
+            } else {
+                el.style.transform = "scale(" +scale+ "," +scale+ ")";
+                requestAnimationFrame(fade);
+            }
+        })();
+    }
+
+    function fadeOutOverlay(el){
         el.style.opacity = 1;
 
         (function fade() {
@@ -49,6 +66,24 @@
 
     // Fade in
     function fadeIn(el, display){
+        el.style.opacity = -1;
+        el.style.display = display || "block";
+        var scale = 1.1;
+
+        (function fade() {
+            var val = parseFloat(el.style.opacity);
+            
+            val += 0.4;
+            scale -= 0.02;
+            if ( val == null || val <= 1 ) {
+                el.style.opacity = val;
+                el.style.transform = "scale(" +scale+ "," +scale+ ")";
+                requestAnimationFrame(fade);
+            }
+        })();
+    }
+
+    function fadeInOverlay(el, display){
         el.style.opacity = 0;
         el.style.display = display || "block";
 
